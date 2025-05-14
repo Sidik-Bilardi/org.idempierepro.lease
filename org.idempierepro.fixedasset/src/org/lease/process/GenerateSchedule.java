950d1cc7cc3d05e412c5e33add72e86ffd8a7b60
Sidik-Bilardi <sidik_bilardi@live.com>
2025-05-14 17:03:46 +0700

    Progress generate schedule

--- org.idempierepro.fixedasset/src/org/lease/process/GenerateSchedule.java
+++ org.idempierepro.fixedasset/src/org/lease/process/GenerateSchedule.java
@@ -1,10 +1,16 @@
 package org.lease.process;
 
 import java.math.BigDecimal;
+import java.sql.Timestamp;
 import java.text.DecimalFormat;
+import java.time.LocalDate;
+import java.time.Period;
+import java.time.YearMonth;
 import java.util.logging.Level;
 
 import org.compiere.model.MAsset;
+import org.compiere.model.MOrder;
+import org.compiere.model.MOrderLine;
 import org.compiere.model.MSequence;
 import org.compiere.model.Query;
 import org.compiere.model.X_AD_Sequence_No;
@@ -12,9 +18,10 @@ import org.compiere.process.ProcessInfoParameter;
 import org.compiere.process.SvrProcess;
 import org.compiere.util.Env;
 import org.compiere.util.Util;
+import org.lease.model.X_C_OrderComponent;
 
 public class GenerateSchedule extends SvrProcess{
-	int p_C_OrderLine_ID = 0;
+	int p_ID = 0;
 	
 	@Override
 	protected void prepare()
@@ -25,16 +32,61 @@ public class GenerateSchedule extends SvrProcess{
 			String name = para[i].getParameterName();
 			if (para[i].getParameter() == null)
 				;
-			else if (name.equals("C_OrderLine_ID"))
-				p_C_OrderLine_ID = (int)para[i].getParameterAsInt();
+//			else if (name.equals("C_OrderLine_ID"))
+//				p_C_OrderLine_ID = (int)para[i].getParameterAsInt();
 			else
 				log.log(Level.SEVERE, "Unknown Parameter: " + name);
 		}
+		p_ID = getRecord_ID();
 		
 	}	//	prepare
 	@Override
 	protected String doIt() throws Exception {
+		String text = "";
+		int count = 0;
+		X_C_OrderComponent comp = new X_C_OrderComponent(getCtx(), p_ID, get_TrxName());
+		MOrder order = new MOrder(getCtx(), comp.getC_Order_ID(), get_TrxName());
+		Timestamp dateStart = comp.getDateStart();
+		Timestamp dateEnd = comp.getEndDate();
+		LocalDate Start = dateStart.toLocalDateTime().toLocalDate();
+		LocalDate End = dateEnd.toLocalDateTime().toLocalDate();
 		
-		return null;
+		for (LocalDate date = Start; date.isBefore(End); date = date.plusMonths(1)) {
+			MOrderLine line = new MOrderLine(getCtx(), 0, get_TrxName());
+	        line.setC_Order_ID(order.get_ID());
+	        line.setM_Product_ID(comp.getM_Product_ID());
+	        line.setQty(Env.ONE);
+	        line.setPrice(Env.ONE);
+	        LocalDate firstDay = date;
+	        if(count > 0 ) {
+	        	 firstDay= date.withDayOfMonth(1);
+	        }
+	        LocalDate lastDay= firstDay.withDayOfMonth(
+	        		firstDay.getMonth().length(firstDay.isLeapYear()));
+	        Period diff = Period.between(firstDay, lastDay);
+	        int BillingDays = diff.getDays();
+	    	
+	        YearMonth yearMonthEnd = YearMonth.of(End.getYear(), End.getMonth());
+	        
+	        YearMonth yearMonthObject = YearMonth.of(lastDay.getYear(), lastDay.getMonth());
+	        int daysInMonth = yearMonthObject.lengthOfMonth(); //28 
+	       
+	        if(yearMonthEnd == yearMonthObject) {
+	        	lastDay = End;
+	        	diff = Period.between(firstDay, lastDay);
+	        	daysInMonth = diff.getDays();
+	        }
+	        String desc = firstDay+" ("+BillingDays+"/"+daysInMonth+") "+lastDay;
+	        line.setDescription(desc);
+	        Timestamp tsLastDay = Timestamp.valueOf(lastDay.atStartOfDay());
+	        Timestamp tsfirstDay = Timestamp.valueOf(firstDay.atStartOfDay());
+	        line.set_ValueOfColumn("EndDate", tsLastDay);
+	        line.set_ValueOfColumn("DateStart", tsfirstDay);
+	        if(line.save()) {
+	        	count++;
+	        }
+	    }
+		
+		return "#"+count+" Generated ";
 	}
 }
